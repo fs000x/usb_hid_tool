8b0b8f9954f1c8eed7bc919f9948653f8a47d863
jakey.chen <281743668@qq.com>
2015-04-28 15:50:16 +0800

    通用类添加注释

--- COM/hidHelper.py
+++ COM/hidHelper.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+win下使用的HID设备通讯帮助类
+'''
+__author__ = "jakey.chen"
+__version__ = "v1.0"
+
+
 import pywinusb.hid as hid
+
 
 class hidHelper(object):
     def __init__(self, vid=0x1391, pid=0x2111):
@@ -12,29 +20,42 @@ class hidHelper(object):
         self.pid = pid
 
     def start(self):
-        try:
-            _filter = hid.HidDeviceFilter(vendor_id = self.vid, product_id = self.pid)
-            hid_device = _filter.get_devices()
+        '''
+        开始，打开HID设备
+        '''
+        _filter = hid.HidDeviceFilter(vendor_id = self.vid, product_id = self.pid)
+        hid_device = _filter.get_devices()
+        if len(hid_device) > 0:
             self.device = hid_device[0]
             self.device.open()
             self.report = self.device.find_output_reports()
             self.alive = True
-        except Exception as e:
-            self.alive = False
 
     def stop(self):
+        '''
+        停止，关闭HID设备
+        '''
         self.alive = False
         if self.device:
             self.device.close()
 
     def setcallback(self):
+        '''
+        设置接收数据回调函数
+        '''
         if self.device:
             self.device.set_raw_data_handler(self.read)
 
     def read(self, data):
-        print [hex(item).upper() for item in data[1:]]
+        '''
+        接收数据回调函数
+        '''
+        print([hex(item).upper() for item in data[1:]])
 
-    def write(self, send_list, timeout=50):
+    def write(self, send_list):
+        '''
+        向HID设备发送数据
+        '''
         if self.device:
             if self.report:
                 self.report[0].set_raw_data(send_list)
@@ -45,9 +66,10 @@ class hidHelper(object):
 if __name__ == '__main__':
     myhid = hidHelper()
     myhid.start()
-    myhid.setcallback()
-    send_list = [0x00 for i in range(65)]
-    myhid.write(send_list)
-    import time
-    time.sleep(0.5)
-    myhid.stop()
+    if myhid.alive:
+        myhid.setcallback()
+        send_list = [0x00 for i in range(65)]
+        myhid.write(send_list)
+        import time
+        time.sleep(0.5)
+        myhid.stop()
--- COM/usbHelper.py
+++ COM/usbHelper.py
@@ -1,39 +1,50 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+linux下使用的usb设备通讯帮助类
+'''
+__author__ = "jakey.chen"
+__version__ = "v1.0"
+
+
 import threading
 import usb.util
 import usb.core
 
-class UsbHelper(object):
+
+class usbHelper(object):
     def __init__(self, vid=0x1391, pid=0x2111):
         self.alive = False
         self.handle = None
+        self.size = 64
         self.vid = vid
         self.pid = pid
 
-    def SetStopEvent(self):
-        self.alive = False
-        self.stop()
-
     def start(self):
-        try:
-            self.dev = usb.core.find(idVendor=self.vid, idProduct=self.pid)
-            if self.dev != None:
-                self.ep_in = self.dev[0][(0,0)][0].bEndpointAddress
-                self.ep_out = self.dev[0][(0,0)][1].bEndpointAddress
-                self.size = self.dev[0][(0,0)][1].wMaxPacketSize
-            self.open()
-            self.alive = True
-        except Exception as e:
-            self.alive = False
+        '''
+        开始，打开usb设备
+        '''
+        self.dev = usb.core.find(idVendor=self.vid, idProduct=self.pid)
+        if self.dev != None:
+            self.ep_in = self.dev[0][(0,0)][0].bEndpointAddress
+            self.ep_out = self.dev[0][(0,0)][1].bEndpointAddress
+            self.size = self.dev[0][(0,0)][1].wMaxPacketSize
+        self.open()
+        self.alive = True
 
     def stop(self):
+        '''
+        停止，关闭usb设备，释放接口
+        '''
         self.alive = False
         if self.handle:
             self.handle.releaseInterface()
 
     def open(self):
+        '''
+        打开usb设备
+        '''
         busses = usb.busses()
         for bus in busses:
             devices = bus.devices
@@ -51,22 +62,33 @@ class UsbHelper(object):
                     except Exception as e:
                         self.alive = False
 
-    def read(self, size=64, timeout=50):
+    def read(self, size=64, timeout=0):
+        '''
+        读取usb设备发过来的数据
+        '''
         if size >= self.size:
             self.size = size
 
         if self.handle:
             data = self.handle.interruptRead(self.ep_in, self.size, timeout)
-            return data.tolist()
 
-    def write(self, send_list, timeout=50):
+        try:
+            data_list = data.tolist()
+            return data_list
+        except:
+            return list()
+
+    def write(self, send_list, timeout=1000):
+        '''
+        发送数据给usb设备
+        '''
         if self.handle:
             bytes_num = self.handle.interruptWrite(self.ep_out, send_list, timeout)
             return bytes_num
 
 if __name__ == '__main__':
     import time
-    dev = UsbHelper()
+    dev = usbHelper()
 
     dev.start()
 
